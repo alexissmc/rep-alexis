8320b7bf14f0cd30ef42ac50e67217117e68ef8b
alexis <asalvadormartinezc@gmail.com>
2023-09-06 14:10:28 -0600

    nuevo cambio

--- src/Component.jsx
+++ src/Component.jsx
@@ -1,25 +1,46 @@
-
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 
 export default function Component() {
+  const [text, setText] = useState('');
+  const [updated, setUpdated] = useState('');
 
-    const [text, setText]=useState()
-    const [updated, setUpdated]=useState()
+  const textOnChange = (event) => {
+    setText(event.target.value);
+  }
 
-    const textOnChange = (event) => {
-        setText(event.target.value)
-    }
-
-    const buttononclick = () => {
-        setUpdated(text)
-    }
+  const buttonOnClick = () => {
+    setUpdated(text);
+  }
 
   return (
     <div>
+      <header>
+        <nav>
+          <ul>
+            <li><a >Inicio</a></li>
+            <li><a >Acerca de</a></li>
+            <li><a >Contacto</a></li>
+          </ul>
+        </nav>
+      </header>
+      <main>
         <input type="text" value={text} onChange={textOnChange} />
-        <button onClick={buttononclick}>Actualizar</button>
+        <button onClick={buttonOnClick}>Actualizar</button>
         <p>Texto input: {text}</p>
         <p>Texto actualizado: {updated}</p>
+        <form>
+          {/* Agrega tus campos de formulario aquí */}
+          <label>
+            Nombre:
+            <input  />
+          </label>
+          <label>
+            Correo electrónico:
+            <input  />
+          </label>
+          <button >Enviar</button>
+        </form>
+      </main>
     </div>
-  )
+  );
 }
